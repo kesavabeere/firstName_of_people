4ab7ba6e8bbeba2b0027725600d41077b26bc05f
kesavabeere <beerekesava007@gmail.com>
2023-01-16 17:12:12 +0000

    firstNames

---
+++ utilities/utils/index.js
@@ -0,0 +1,6 @@
+const list = require("../country/state/city/index");
+const getFirstNames = (list) => {
+  return list.map((eachPerson) => eachPerson.firstName);
+};
+
+module.exports = getFirstNames;
